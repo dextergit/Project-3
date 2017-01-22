7d30cdcbab7247f612c5072cd7f3a0b07cb9ceba
Dexter Hechanova <dexter@fastmail.jp>
2017-01-22 00:24:05 -0800

    Initial project setup.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,10 @@
 import media
 
-toy_story = media.Movie("Toy Story", "Toys come to life.", "img", "youtuble")
+toy_story = media.Movie("Toy Story", "Toys come to life.", "img", "http://www.cnn.com")
 
 print (toy_story.storyline)
+
+#toy_story.show_trailer()
+
+#print (toy_story.__name__)
+print (media.Movie.__name__)
