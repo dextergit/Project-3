d981365f315016b233f7534b88ab93f9128eb404
Dexter Hechanova <dexter@fastmail.jp>
2017-01-28 20:14:53 -0800

    Finishing up the project.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,33 +1,13 @@
 import media
 import fresh_tomatoes
 
-# REFERENCES:
-
+# References
 # http://stackoverflow.com/questions/14676265/how-to-read-text-file-into-a-list-or-array-with-python
-# Udacity class + zip attachments
+# Udacity class content, including videos, attachment, etc.
+# YouTube (for trailers)
+# Wikipedia (for images)
 
-# toy_story1 = media.Movie("Toy Story", "Toys come to life.", "https://images-na.ssl-images-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://www.youtube.com/watch?v=ZZv1vki4ou4")
-# toy_story2 = media.Movie("Toy Story", "Toys come to life.", "https://images-na.ssl-images-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://www.youtube.com/watch?v=ZZv1vki4ou4")
-# toy_story3 = media.Movie("Toy Story", "Toys come to life.", "https://images-na.ssl-images-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://www.youtube.com/watch?v=ZZv1vki4ou4")
-#
-#
-# #print (toy_story.storyline)
-# #toy_story.show_trailer()
-# #print (toy_story.__name__)
-# #print (media.Movie.__name__)
-#
-# movies = []
-#
-# movies.append(toy_story1)
-# movies.append(toy_story2)
-# movies.append(toy_story3)
-
-# fresh_tomatoes.open_movies_page(movies)
-
-#for movie in movies:
-#	print movie.storyline
-
-
+# >> Function - Gets a list of movies from a file, creates instances of the Movie object, and returns an array of Movie instances.
 def get_movies(movie_file):
 	movies_file = open(movie_file, 'r')
 	movie_inputs = movies_file.readlines()
